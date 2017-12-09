f3837e5dbc80773d8cd1f309d690649de193ff08
Mtgottschalk <mattg5555@gmail.com>
2017-12-09 10:27:22 -0700

    Consolidating code

--- addpizzastocart.php
+++ addpizzastocart.php
@@ -6,59 +6,65 @@ require_once("Cart.Class.php");
 $pizza = new Pizza();
 $name = $pizza->getNameByToppings($_POST["pizzaselect"]);
 $price = 0.0;
+$addprice = 0.0;
 $quanity = $_POST["quanity"];
 //echo($_POST["pizzaselect"]);
 //echo($_POST["size"]);
-if($_POST["size"]=="slice")$price = $pizza->getPriceslByName($name);
-else if($_POST["size"]=="small")$price = $pizza->getPricesByName($name);
-else if($_POST["size"]=="medium")$price = $pizza->getPricembyName($name);
-else $price = $pizza->getPricelByName($name);
+if($_POST["size"]=="slice")
+{
+	$price = $pizza->getPriceslByName($name);
+	$addprice = .25;
+}
+else if($_POST["size"]=="small")
+{
+	$price = $pizza->getPricesByName($name);
+	$addprice = .5;
+}
+else if($_POST["size"]=="medium")
+{
+	$price = $pizza->getPricembyName($name);
+	$addprice = 1;
+}
+else 
+{
+	$price = $pizza->getPricelByName($name);
+	$addprice = 1.50;
+}
 
 $price = $price*$quanity;
-
+$ec = false;
 
 
 echo ('<h1>Size = '.$_POST["size"].'</h1></html>');
 echo ('<h1>Name = '.$name.'</h1>');
- echo ('<h1>Initial toppings = '.$_POST["pizzaselect"].'</h1>');
- echo ('<h1>Actual toppings = '.$_POST["alltoppings"].'</h1>');
-echo ('<html><h1>Price = '.$price.'</h1>');
+// echo ('<h1>Initial toppings = '.$_POST["pizzaselect"].'</h1>');
+ echo ('<h1>toppings = '.$_POST["alltoppings"].'</h1>');
+echo ('<html><h1>Price before toppings= '.$price.'</h1>');
 var_dump($_POST);
 
 $alltoppings = array();
-$initialtoppings = array();
 $temp = "";
 for($i = 0; $i<strlen($_POST["alltoppings"]);$i++) 
 if($_POST["alltoppings"][$i]!= ",") $temp .= $_POST["alltoppings"][$i];
 else
 {
 	array_push($alltoppings, $temp);
+	if($temp == "Extra Cheese") $ec = true;
 	$temp = "";
 }
 array_push($alltoppings, $temp);
+if($temp == "Extra Cheese") $ec = true;
 	$temp = "";
-	
-	$initialtoppings = array();
-$temp = "";
-for($i = 0; $i<strlen($_POST["pizzaselect"]);$i++) 
-if($_POST["pizzaselect"][$i]!= " ") $temp .= $_POST["pizzaselect"][$i];
-else
-{
-	array_push($initialtoppings, $temp);
-	$temp = "";
+ if($ec == true) $price += $addprice;	
+if(count($alltoppings)>= 4) $price += $addprice *4;
+else { if(ec == true) $price += count($alltoppings)-1 * $addprice;
+	else $price += count($alltoppings) * $addprice;
 }
-array_push($initialtoppings, $temp);
-	$temp = "";
-	
-$count = 0;
-for($i=0; $i < count($alltoppings);$i++)
-for($j=0; $j < count($initialtoppings);$j++)
-if($initialtoppings[$j] == $alltoppings[$i]) $count++;
-$additionaltoppings = count($alltoppings) - $count;
-echo('Number of additional toppings = '.$additionaltoppings);
+
+echo('Number of toppings = '.$alltoppings);
 
 $cart = new Cart();
-
+echo ('<html><h1>Price after toppings= '.$price.'</h1>');
 $cart->addPrice($price);
 $cart->addName($name);
 $cart->addDescription('Pizza w/'.$_POST["alltoppings"]);
@@ -66,5 +72,27 @@ $cart->addQuanity($quanity);
 
 //$cart->addQuanity($quanity);
 
-header('Location: ./menu.php');
+//header('Location: ./menu1.php');
+//.5 sm 1 med 1.50 large pizza extra cheese always costs 
+//.25 half .50 full subs
+//
+/*$temp = "";
+for($i = 0; $i<strlen($_POST["pizzaselect"]);$i++) 
+if($_POST["pizzaselect"][$i]!= ",") $temp .= $_POST["pizzaselect"][$i];
+else
+{
+	array_push($initialtoppings, $temp);
+	$temp = "";
+}
+array_push($initialtoppings, $temp);
+	$temp = "";
+
+$count = 0;
+for($i=0; $i < count($alltoppings);$i++)
+for($j=0; $j < count($initialtoppings);$j++)
+if($initialtoppings[$j] == $alltoppings[$i]) $count++;
+$additionaltoppings = count($alltoppings) - $count;
+*/
 ?>
+
+
--- addsubtocart.php
+++ addsubtocart.php
@@ -5,15 +5,66 @@ require_once("Sub.Class.php");
 require_once("Cart.Class.php");
 require_once("session.class.php");
 
-
+$ec = false;
 $sub =  new Sub();
 $name = $_POST["name"];
 $size = $_POST["size"];
 $price = 0.0;
+$toppings = $_POST["allsubtoppings"];
+echo($toppings);
 $quanity = $_POST["quanity"];
-if($size=="Half")$price = $sub->getSubPriceHalf($name);
-if($size=="Full")$price = $sub->getSubPriceFull($name);
-
+if($size=="Half")
+{
+	$addprice = .25;
+	$price = $sub->getSubPriceHalf($name);
+	echo($price);
+}
+if($size=="Full")
+{
+	$addprice = .50;
+	$price = $sub->getSubPriceFull($name);
+	echo($price);
+}
+echo($addprice);
+$alltoppings = array();
+$temp = "";
+for($i = 0; $i<strlen($_POST["allsubtoppings"]);$i++) 
+if($_POST["allsubtoppings"][$i]!= ",") $temp .= $_POST["allsubtoppings"][$i];
+else
+{
+	array_push($alltoppings, $temp);
+	if($temp == "extracheese")
+	{
+		 $price+= $addprice;
+		 echo("extracheese");
+	 }
+	if($temp == "hotpeppers")
+	{
+		 $price+= $addprice;
+		 echo("hotpeppers");
+	 }
+	if($temp == "sweetpeppers") 
+	{
+		 $price+= $addprice;
+		 echo("sweetpeppers");
+	 }
+	$temp = "";
+}
+if($temp == "extracheese")
+	{
+		 $price+= $addprice;
+		 echo("extracheese");
+	 }
+	if($temp == "hotpeppers")
+	{
+		 $price+= $addprice;
+		 echo("hotpeppers");
+	 }
+	if($temp == "sweetpeppers") 
+	{
+		 $price+= $addprice;
+		 echo("sweetpeppers");
+	 }
 echo ('Name = '.$name.'</br>');
 echo ('Price = '.$price.'</br>');
 echo ('Size = '.$size.'</br>');
@@ -23,14 +74,14 @@ $cart = new Cart();
 
 $cart->addPrice($price);
 $cart->addName($name);
-$cart->addDescription("Sub ");
+$cart->addDescription("Sub w/".$toppings);
 $cart->addQuanity($quanity);
 
 
 var_dump($_SESSION["cart"]["prices"]);
 var_dump($_SESSION["cart"]["names"]);
 
-header('Location: ./menu.php');
+//header('Location: ./menu1.php');
 
 
 
